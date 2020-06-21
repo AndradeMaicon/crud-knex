2927b9d0042fac5f7c38ff1845441b2501dcea11
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-06-21 15:54:35 -0300

    pagination created

--- src/controller/ProjectController.js
+++ src/controller/ProjectController.js
@@ -3,17 +3,26 @@ const knex = require('../database')
 module.exports = {
   async index (req , res, next) {
     try {
-      const { user_id } = req.query;
-      
+      const { user_id , page = 1 } = req.query;
       
       const query = knex('projects')
+        .limit(5)
+        .offset((page - 1) * 5)
+
+      const countObj = knex('projects').count()
 
       if (user_id) {
         query
           .where({ user_id })
           .join('users', 'users.id', '=', 'projects.user_id')
           .select('projects.*', 'users.username')
+
+        countObj
+          .where({ user_id })
       }
+
+      const [ count ] = await countObj
+      res.header('X-Total-Cont', count["count"])
 
       const results = await query
 
@@ -23,5 +32,21 @@ module.exports = {
     }
   },
 
+  async store (req, res, next) {
+    try {
+      const { user_id, title } = req.body
+
+      await knex('projects').insert({
+        user_id,
+        title
+      })
+
+      res.status(201).send()
+    } catch (error) {
+      next(error)
+    }
+
+    return res.send()
+  },
 
 }
